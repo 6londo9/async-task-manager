6b0b6c2e32504654a3c19d83b001acb4be6b9633
Ilya Ostapenko <6londo9@gmail.com>
2026-06-05 17:37:50 +0400

    Init commit

---
+++ src/main/java/com/backendDojo/asyncTaskManager/config/DataConfig.java
@@ -0,0 +1,21 @@
+package com.backendDojo.asyncTaskManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DataConfig {
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ src/test/java/com/backendDojo/asyncTaskManager/AsyncTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendDojo.asyncTaskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTaskManagerApplicationTests extends AbstractTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
